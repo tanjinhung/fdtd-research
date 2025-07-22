fe8a6f18395eb1a31ce3256a1581a31ab3f7f2b9
tanjinhung <tanjinhung2013@gmail.com>
2025-07-23 06:45:20 +0900

    Initiated Main

--- fdtd.c
+++ fdtd.c
@@ -1,1 +1,9 @@
+#define NOB_IMPLEMENTATION
+#include "nob.h"
 
+int main(int argc, char *argv[]) {
+  printf(argc > 1 ? "Hello, %s!\n" : "Hello, World!\n",
+         argc > 1 ? argv[1] : "");
+  printf("Hello, FDTD World!\n");
+  return 0;
+}
