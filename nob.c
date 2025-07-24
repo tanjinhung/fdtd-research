87c13d1a32cf57b8350ba23c6956cbce260d2a5f
tanjinhung <tanjinhung2013@gmail.com>
2025-07-24 14:19:00 +0900

    Implemented raylib.h

--- nob.c
+++ nob.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
   nob_cc_flags(&cmd);
   nob_cc_output(&cmd, BUILD_FOLDER "fdtd");
   nob_cc_inputs(&cmd, SRC_FOLDER "fdtd.c");
-  nob_cmd_append(&cmd, "-lm");
+  nob_cmd_append(&cmd, "-lm", "-lraylib");
 
   if (!nob_cmd_run_sync_and_reset(&cmd))
     return 1;
--- src/fdtd.c
+++ src/fdtd.c
@@ -2,19 +2,13 @@
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #include "../nob.h"
+#include "raylib.h"
 
 #define SIZE 200
-#define MAX_TIME 1000
+#define MAX_TIME 250
 
-int main() {
-  double ez[SIZE] = {0.};
-  double hy[SIZE] = {0.};
-  double impedence0 = 377.0;
+void simulate_fdtd(double ez[], double hy[], double impedence0) {
   int qTime, mm;
-
-  // hy[m] = hy[m] + (ez[m + 1] - ez[m]) / impedence0;
-  // ez[m] = ez[m] + (hy[m] - hy[m - 1]) * impedence0;
-
   for (qTime = 0; qTime < MAX_TIME; qTime++) {
     for (mm = 0; mm < SIZE - 1; mm++) {
       hy[mm] += (ez[mm + 1] - ez[mm]) / impedence0;
@@ -24,11 +18,37 @@ int main() {
       ez[mm] += (hy[mm] - hy[mm - 1]) * impedence0;
     }
 
-    ez[0] += exp(-(qTime - 30.) * (qTime - 30.) / 100.0);
+    ez[0] = exp(-(qTime - 30.) * (qTime - 30.) / 100.0);
 
-    printf("Time step %d: ", qTime);
-    printf("%g\n", ez[50]);
+    if (qTime % 10 == 0) {
+      printf("Time step %d: ", qTime);
+      printf("%g\n", ez[50]);
+    }
   }
+}
+
+int main() {
+  double ez[SIZE] = {0.};
+  double hy[SIZE] = {0.};
+  double impedence0 = 377.0;
+  const int screenWidth = 800;
+  const int screenHeight = 600;
+
+  InitWindow(screenWidth, screenHeight, "FDTD Simulation");
+  SetTargetFPS(60);
+
+  // hy[m] = hy[m] + (ez[m + 1] - ez[m]) / impedence0;
+  // ez[m] = ez[m] + (hy[m] - hy[m - 1]) * impedence0;
+  simulate_fdtd(ez, hy, impedence0);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawFPS(10, 10);
+    EndDrawing();
+  }
+
+  CloseWindow();
 
   return 0;
 }
